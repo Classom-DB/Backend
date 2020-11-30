6c19e970ef29751605c026859b76f9c90a3c593b
AntsThatWander <anubspider@gmail.com>
2020-12-01 01:38:16 +0900

    Modify Hotel Prefix

--- routes/employee.js
+++ routes/employee.js
@@ -12,26 +12,26 @@ router.get('/get', async (req, res) => {
     const isId = (id == undefined) ? false : true;
     const isDN = (dept_name == undefined) ? false : true;
 
-    const infoQuery = `, max_guest from hotel.employee, hotel.info where hotel.info.emp_id = hotel.employee.id`
-    const deptQuery = `, facility_name from hotel.employee, hotel.management where hotel.management.emp_id = hotel.employee.id`
+    const infoQuery = `, max_guest from employee, info where info.emp_id = employee.id`
+    const deptQuery = `, facility_name from employee, management where management.emp_id = employee.id`
     
     try {
         if(isDN === true){
             if(dept_name == "info") query += infoQuery
             else if(dept_name == "facility") query += deptQuery
-            else query += `from hotel.employee where dept_name = '${dept_name}'`
+            else query += `from employee where dept_name = '${dept_name}'`
 
             if(isId === true) query += ` and id = '${id}'`
         } else if(isId === true) {
-            const tmp_query = `select dept_name from hotel.employee where id = '${id}'`
+            const tmp_query = `select dept_name from employee where id = '${id}'`
             const result = await db.dbQuery(tmp_query)
 
-            if(result[0]['dept_name'] == "info") query += infoQuery + ` and hotel.employee.id = '${id}'`
-            else if(result[0]['dept_name'] == "facility") query += deptQuery +` and hotel.employee.id = '${id}'`
-            else query += ` from hotel.employee where id = '${id}'`
+            if(result[0]['dept_name'] == "info") query += infoQuery + ` and employee.id = '${id}'`
+            else if(result[0]['dept_name'] == "facility") query += deptQuery +` and employee.id = '${id}'`
+            else query += ` from employee where id = '${id}'`
             
         }
-        else query += ` from hotel.employee`
+        else query += ` from employee`
 
     
         const result = await db.dbQuery(query)
@@ -47,10 +47,10 @@ router.post('/add', async (req, res) => {
     const data = req.body;
     try {
         console.log(data.id)
-        let query = `select id from hotel.employee where id = '${data.id}'`
+        let query = `select id from employee where id = '${data.id}'`
         const check = await db.dbQuery(query)
         if (check.length !== 0) throw 'id exists'
-        query = `insert into hotel.employee values('${data.id}', '${data.password}', 
+        query = `insert into employee values('${data.id}', '${data.password}', 
             '${data.first_name}', '${data.last_name}', '${data.phone_number}', '${data.address}', '${data.email}', '${data.gender}', '${data.salary}', '${data.dept_name}', 
             '${data.position}', '${data.birth}', '${data.join_date}')`
         const result = await db.dbQuery(query)
--- routes/parking.js
+++ routes/parking.js
@@ -8,7 +8,7 @@ router.get('/guest/get/:floor', async (req, res) => {
     const { floor } = req.params
     console.log(floor)
     try {
-        const query = `select id, section, emp_id, guest_id from hotel.parking where floor = ${floor}`
+        const query = `select id, section, emp_id, guest_id from parking where floor = ${floor}`
         const result = await db.dbQuery(query)
 
         if (result === null) {
@@ -24,7 +24,7 @@ router.get('/employee/get/:emp_id', async (req, res) => {
     const { emp_id } = req.params
     console.log(emp_id)
     try {
-        const query = `select id, section, guest_id, floor from hotel.parking where emp_id = '${emp_id}'`
+        const query = `select id, section, guest_id, floor from parking where emp_id = '${emp_id}'`
         const result = await db.dbQuery(query)
         if (result === undefined) {
             res.json({"data" : "none", "code" : 200, "timestamp" : new Date().getDate()})
@@ -39,7 +39,7 @@ router.post('/add', async (req, res) => {
     const data = req.body
 
     try {
-        const query = `insert into hotel.parking values(default, '${data.section}', ${data.floor}, '${data.emp_id}', '${data.guest_id}')`
+        const query = `insert into parking values(default, '${data.section}', ${data.floor}, '${data.emp_id}', '${data.guest_id}')`
         const result = await db.dbQuery(query)
         if(result === undefined || result === null) throw 'query error'
         res.json({"data" : "succuess", "code" : 200, "timestamp" : new Date().getDate()})
@@ -52,7 +52,7 @@ router.delete('/delete', async (req, res) => {
     const id = req.query.id
     console.log(id)
     try {
-        const query = `delete from hotel.parking where id = ${id}`
+        const query = `delete from parking where id = ${id}`
         const result = await db.dbQuery(query)
         if (result === undefined) throw 'query error'
     } catch(err) {
