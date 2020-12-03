f83feeda6c44deaf4f74a80cbe0aee302c628cab
hsmint <hsmint.hong@gmail.com>
2020-12-03 23:28:17 +0900

    Allow method for put and delete

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET", "POST");
+  res.header("Access-Control-Allow-Methods", "GET", "POST", "DELETE", "PUT");
   res.header("Access-Control-Allow-Headers", "content-type");
   next();
 });
