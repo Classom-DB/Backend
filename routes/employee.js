b82e3731049bbeef29cb29df4e45705dcc5120d2
AntsThatWander <anubspider@gmail.com>
2020-12-04 01:49:15 +0900

    modify employee/put

--- routes/employee.js
+++ routes/employee.js
@@ -47,9 +47,14 @@ router.post('/add', async (req, res) => {
     const data = req.body;
     try {
         console.log(data.id)
-        let query = `select id from employee where id = '${data.id}'`
+        let query = `select id, dept_name from employee where id = '${data.id}'`
         const check = await db.dbQuery(query)
         if (check.length !== 0) throw 'id exists'
+
+        if (check[0].dept_name === 'info'){
+            query = `insert into info values ('${data.id}', 9)`
+        }
+
         query = `insert into employee values('${data.id}', '${data.password}', 
             '${data.first_name}', '${data.last_name}', '${data.phone_number}', '${data.address}', '${data.email}', '${data.gender}', '${data.salary}', '${data.dept_name}', 
             '${data.position}', '${data.birth}', '${data.join_date}')`
@@ -85,22 +90,41 @@ router.delete('/delete', async (req, res) => {
     }
 })
 
-// router.put('/change', async (req, res) => {
-//     const query = req.query;
-//     const data = req.body;
-//     try {
-//         let sqlStr = `select id from employee where id = '${query.id}'`
-//         const check = await db.dbQuery(sqlStry)
-//         if (check.length === 0) throw 'not exists'
+router.put('/change', async (req, res) => {
+    const query = req.query;
+    const data = req.body;
 
-//         sqlStr = `update employee `
-//         const result = await db.dbQuery(sqlStr)
-//         if (result === null) throw 'query error'
-//         res.json({"data": "success", "code": 200, "timestamp": new Date().getDate()})
-//     } catch (error) {
-//         console.log(error)
-//         res.json({"data": error, "code": 404, "timestamp": new Date().getDate()})
-//     }
-// })
+    try {
+        let sqlStr = `select id, dept_name from employee where id = '${query.id}'`
+        const check = await db.dbQuery(sqlStr)
+        if (check.length === 0) throw 'not exists'
+        if (check[0].dept_name === "info" && data.dept_name !== "info") {
+            sqlStr = `delete from info where emp_id = '${query.id}'`
+            const subcheck = await db.dbQuery(sqlStr)
+            if(subcheck === null) throw 'query error'
+        }
+
+        sqlStr = `update employee set 
+        first_name = '${data.first_name}', 
+        last_name = '${data.last_name}', 
+        phone_number = '${data.phone_number}',
+        address = '${data.address}', 
+        email = '${data.email}', 
+        gender = '${data.gender}', 
+        salary = ${data.salary}, 
+        dept_name = '${data.dept_name}', 
+        position = '${data.position}',
+        birth = '${data.birth}',
+        join_date = '${data.join_date}'
+        where id = '${query.id}'`
+        const result = await db.dbQuery(sqlStr)
+        if (result === null) throw 'query error'
+        res.json({"data": "success", "code": 200, "timestamp": new Date().getDate()})
+
+    } catch (error) {
+        res.status(404)
+        res.json({"data": error, "code": 404, "timestamp": new Date().getDate()})
+    }
+})
 
 module.exports = router;
