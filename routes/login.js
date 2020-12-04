ca24063834f3d9d690a35ef8d2b261417ac63d4a
hsmint <hsmint.hong@gmail.com>
2020-12-04 21:33:19 +0900

    fix login

--- routes/login.js
+++ routes/login.js
@@ -9,9 +9,10 @@ router.get('/guest', async (req, res) => {
     try {
         const sqlStr = `select password from guest where id = '${id}'`
         const result = await db.dbQuery(sqlStr)
-        if (result === null && result === undefined) throw 'null data'
+        if (result === null || result === undefined) throw 'null data'
         res.json(template.jsonCreate(result))
     } catch(err) {
+        res.status(404);
         res.json({"code": 404, "timestamp": new Date().getDate()})
     }
 })
@@ -24,6 +25,7 @@ router.get('/employee', async (req, res) => {
         if (result === null && result === undefined) throw 'null data'
         res.json(template.jsonCreate(result))
     } catch(err) {
+        res.status(404);
         res.json({"code": 404, "timestamp": new Date().getDate()})
     }
 })
