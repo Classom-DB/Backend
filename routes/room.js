d65abc75c72eba069d605145384c149b8ab39fae
AntsThatWander <anubspider@gmail.com>
2020-12-09 18:12:38 +0900

    modify room/get/peremp

--- routes/room.js
+++ routes/room.js
@@ -8,7 +8,8 @@ let router = express.Router();
 router.get('/get/perroom', async (req, res) => {
     const data = req.query
     const query = `select guest.first_name as guest_first_name, guest.last_name as guest_last_name, employee.first_name as emp_first_name, employee.last_name as emp_last_name, 
-    guest.gender, grade, mileage, year, month, day, guest.phone_number, guest.email, guest.address 
+    guest.gender, 
+    grade, mileage, year, month, day, guest.phone_number, guest.email, guest.address 
     from guest
     join reserved on guest.id = reserved.guest_id
     join room on reserved.room_num = room.number
@@ -26,13 +27,24 @@ router.get('/get/perroom', async (req, res) => {
 })
 
 router.get('/get/peremp', async (req, res) => {
-    const query = `select id, first_name, last_name, number from employee, room where employee.id = room.emp_id`
+    const query = `select distinct id, first_name, last_name from employee, room where employee.id = room.emp_id`
 
     try {
         const result = await db.dbQuery(query)
+
+        for(var idx in result){
+            const subquery = `select number from room where emp_id = '${result[idx].id}'`;
+            const subresult = await db.dbQuery(subquery);
+
+            let tmp = [];
+            for(var i in subresult){
+                tmp.push(subresult[i].number + "호");
+            }
+            result[idx].number = tmp;
+        }
+
         if (Object.keys(result) === 0) throw 'null data'
         if (result === null) throw 'query error'
-        res.json(template.jsonCreate(result))
     } catch(err) {
         res.json({"code": 404, "timestamp": new Date().getDate()})
     }
